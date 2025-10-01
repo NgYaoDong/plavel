8972cf385139b653f67cb40654814c4287fb105d
NgYaoDong <e0957740@u.nus.edu>
2025-10-01 18:40:35 +0800

    feat: implement file upload functionality with Uploadthing integration

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        hostname: "512fuplqe7.ufs.sh",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
